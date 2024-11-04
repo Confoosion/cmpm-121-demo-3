ac93716876b64716e6e5128f9b58a177b2e7ed2d
Jake Sales <jfsales@ucsc.edu>
2024-11-03 17:53:02 -0800

    L16 Dialog
    Added button that gives a pop-up message saying "You clicked the button!"

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 // todo
+function createButton() {
+  const button = document.createElement("button");
+
+  button.textContent = "Click Me";
+
+  button.addEventListener("click", () => {
+    alert("You clicked the button!");
+  });
+
+  document.body.appendChild(button);
+}
+
+createButton();
